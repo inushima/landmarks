59e9e9394fb3c9d6bc1bdadba61769a0681bcf59
inushima <inushima@arcadit.co.jp>
2023-07-25 17:22:45 +0900

    complete tutorial

---
+++ Landmarks/Views/Landmarks/LandmarkList.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+
+struct LandmarkList: View {
+    @EnvironmentObject var modelData: ModelData
+    @State private var showFavoritesOnly = false
+
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            !showFavoritesOnly || landmark.isFavorite
+        }
+    }
+
+    var body: some View {
+        NavigationView {
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+                .navigationTitle("Landmarks")
+            }
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var landmarks = ModelData().landmarks
+
+    static var previews: some View {
+        ForEach(["iPhone SE (3rd generation)",
+                 "iPhone 13 mini",
+                ], id: \.self) { deviceName in
+            LandmarkList()
+                .environmentObject(ModelData())
+        }
+    }
+}
